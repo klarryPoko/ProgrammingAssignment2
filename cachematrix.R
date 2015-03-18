3a7743c075983f1949a24410a197fe9af5670032
Klpokorny <Klpokorny@mckendree.edu>
2015-03-18 11:43:25 -0500

    Maybe this one will work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These functions allow for the caching of matrix inversion to help speed things up
+## The first function does the work and returns a list of the functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y){
+            x<<-y
+            m<<- NULL
+      }
+      get <- function() x
+      setInv <- function (inv) m <<- solve
+      getInv <- function() m
+      list(set =set, get = get, setInv = setInv, getInv = getInv )
 }
 
 
-## Write a short comment describing this function
+
+## this function will check if the answer is already cached.
+## if not it will then call to have the inverse calculated. 
+## if so it will sue the cached version.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       m <- x$getInv()
+       if(!is.null(m)){
+             message("getting the chashed data")
+             return (m)
+       }
+       data<- x$get()
+       m <- solve(data, ...)
+       x$setInv(m)
+       m
 }
